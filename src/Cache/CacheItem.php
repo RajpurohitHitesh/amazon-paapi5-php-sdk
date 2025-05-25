93c94110e80cd9681ba23748c94359e327a9e538
Hitesh Rajpurohit <iamrajpurohithitesh@gmail.com>
2025-05-25 13:26:47 +0530

    Update CacheItem.php

--- src/Cache/CacheItem.php
+++ src/Cache/CacheItem.php
@@ -6,151 +6,85 @@ namespace AmazonPaapi5\Cache;
 
 use Psr\Cache\CacheItemInterface;
 
-if (PHP_VERSION_ID >= 80000) {
-    class CacheItem implements CacheItemInterface
+class CacheItem implements CacheItemInterface
+{
+    private string $key;
+    /** @var mixed */
+    private $value;
+    private bool $hit;
+    private ?\DateTimeInterface $expiry = null;
+
+    /**
+     * @param mixed $value
+     */
+    public function __construct(string $key, $value = null, bool $hit = false)
     {
-        private string $key;
-        /** @var mixed */
-        private $value;
-        private bool $hit;
-        private ?\DateTimeInterface $expiry = null;
-
-        /**
-         * @param mixed $value
-         */
-        public function __construct(string $key, $value = null, bool $hit = false)
-        {
-            $this->key = $key;
-            $this->value = $value;
-            $this->hit = $hit;
-        }
-
-        public function getKey(): string
-        {
-            return $this->key;
-        }
-
-        public function get(): mixed
-        {
-            return $this->value;
-        }
-
-        public function isHit(): bool
-        {
-            return $this->hit;
-        }
-
-        public function set(mixed $value): static
-        {
-            $this->value = $value;
-            $this->hit = true;
-            return $this;
-        }
-
-        public function expiresAt(?\DateTimeInterface $expiration): static
-        {
-            $this->expiry = $expiration;
-            return $this;
-        }
-
-        public function expiresAfter(int|\DateInterval|null $time): static
-        {
-            if ($time === null) {
-                $this->expiry = null;
-            } elseif ($time instanceof \DateInterval) {
-                $this->expiry = (new \DateTime())->add($time);
-            } elseif (is_int($time)) {
-                $this->expiry = (new \DateTime())->add(new \DateInterval("PT{$time}S"));
-            }
-            return $this;
-        }
-
-        public function getExpiry(): ?\DateTimeInterface
-        {
-            return $this->expiry;
-        }
+        $this->key = $key;
+        $this->value = $value;
+        $this->hit = $hit;
     }
-} else {
-    class CacheItem implements CacheItemInterface
+
+    public function getKey(): string
     {
-        private string $key;
-        /** @var mixed */
-        private $value;
-        private bool $hit;
-        private ?\DateTimeInterface $expiry = null;
+        return $this->key;
+    }
 
-        /**
-         * @param mixed $value
-         */
-        public function __construct(string $key, $value = null, bool $hit = false)
-        {
-            $this->key = $key;
-            $this->value = $value;
-            $this->hit = $hit;
-        }
+    /**
+     * @return mixed
+     */
+    #[\ReturnTypeWillChange]
+    public function get()
+    {
+        return $this->value;
+    }
 
-        public function getKey(): string
-        {
-            return $this->key;
-        }
+    public function isHit(): bool
+    {
+        return $this->hit;
+    }
 
-        /**
-         * @return mixed
-         */
-        #[\ReturnTypeWillChange]
-        public function get()
-        {
-            return $this->value;
-        }
+    /**
+     * @param mixed $value
+     * @return self
+     */
+    #[\ReturnTypeWillChange]
+    public function set($value)
+    {
+        $this->value = $value;
+        $this->hit = true;
+        return $this;
+    }
 
-        public function isHit(): bool 
-        {
-            return $this->hit;
-        }
+    /**
+     * @param \DateTimeInterface|null $expiration
+     * @return self
+     */
+    #[\ReturnTypeWillChange]
+    public function expiresAt($expiration)
+    {
+        $this->expiry = $expiration;
+        return $this;
+    }
 
-        /**
-         * @param mixed $value
-         * @return self
-         */
-        #[\ReturnTypeWillChange]
-        public function set($value)
-        {
-            $this->value = $value;
-            $this->hit = true;
-            return $this;
+    /**
+     * @param \DateInterval|int|null $time
+     * @return self
+     */
+    #[\ReturnTypeWillChange]
+    public function expiresAfter($time)
+    {
+        if ($time === null) {
+            $this->expiry = null;
+        } elseif ($time instanceof \DateInterval) {
+            $this->expiry = (new \DateTime())->add($time);
+        } elseif (is_int($time)) {
+            $this->expiry = (new \DateTime())->add(new \DateInterval("PT{$time}S"));
         }
+        return $this;
+    }
 
-        /**
-         * @param \DateTimeInterface|null $expiration
-         * @return self
-         */
-        #[\ReturnTypeWillChange]
-        public function expiresAt($expiration)
-        {
-            $this->expiry = $expiration;
-            return $this;
-        }
-
-        /**
-         * @param \DateInterval|int|null $time
-         * @return self
-         */
-        #[\ReturnTypeWillChange]
-        public function expiresAfter($time)
-        {
-            if ($time === null) {
-                $this->expiry = null;
-            } elseif ($time instanceof \DateInterval) {
-                $this->expiry = (new \DateTime())->add($time);
-            } elseif (is_int($time)) {
-                $this->expiry = (new \DateTime())->add(new \DateInterval("PT{$time}S"));
-            }
-            return $this;
-        }
-
-        public function getExpiry(): ?\DateTimeInterface
-        {
-            return $this->expiry;
-        }
+    public function getExpiry(): ?\DateTimeInterface
+    {
+        return $this->expiry;
     }
 }
